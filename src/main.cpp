1f82bf7b293e1e0402731c80f4db20cb48cd1204
BooGer <bugrimov@pm.me>
2019-04-12 15:42:00 +1000

    add files from project folder

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+#include "BME280.h"
+
+Serial pc(USBTX, USBRX);
+DigitalOut led(LED1);
+
+
+BME280 sensor(I2C_SDA, I2C_SCL);
+
+
+int main() {
+    
+    while(1) {
+        pc.printf("%2.2f degC, %04.2f hPa, %2.2f %%\n", sensor.getTemperature(), sensor.getPressure(), sensor.getHumidity());
+        led = !led;
+        wait(1);
+    }
+}
