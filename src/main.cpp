9c80945f5ffa428e0bbc7b1e6be58bda84e6b460
BooGer <bugrimov@pm.me>
2019-04-13 13:33:48 +1000

    try to off serial
    для уменьшения занимаемой памяти
    PROGRAM: [======    ]  62.7% (used 41068 bytes from 65536 bytes)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 #include "BME280.h"
 
-Serial pc(USBTX, USBRX);
+// Serial pc(USBTX, USBRX);
 DigitalOut led(LED1);
 
 
@@ -9,10 +9,11 @@ BME280 sensor(I2C_SDA, I2C_SCL);
 
 
 int main() {
-    
     while(1) {
-        pc.printf("%2.2f degC, %04.2f hPa, %2.2f %%\n", sensor.getTemperature(), sensor.getPressure(), sensor.getHumidity());
-        led = !led;
-        wait(1);
+        // pc.printf("%2.2f degC, %04.2f hPa, %2.2f %%\n", sensor.getTemperature(), sensor.getPressure(), sensor.getHumidity());
+        if(sensor.getHumidity() && sensor.getPressure() && sensor.getTemperature()){
+             led = !led;
+             }
+        wait(0.5);
     }
 }
